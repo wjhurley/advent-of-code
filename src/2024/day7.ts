e3b43e0601824767828f064b6094314ec6bef2a0
William Hurley <wjhurley@comcast.net>
2024-12-27 02:04:47 -0800

    Add solution and associated tests for 2024 Day 7 part 2

--- src/2024/day7.ts
+++ src/2024/day7.ts
@@ -31,6 +31,30 @@ export const getCombinations = (operatorCount: number): string[][] => {
     }
 };
 
+export const getCombinations2 = (operatorCount: number): string[][] => {
+    const operators = [ '+', '*', '||' ];
+    const arr: string[][] = [ ...new Array(operatorCount) ].map(_ => operators);
+
+    // When array contains 1 element
+    if (operatorCount == 1) {
+        return operators.map(s => ([ s ]));
+    } else {
+        const combinations = [];
+        const newCount = operatorCount - 1;
+
+        // Recur with the rest of the array.
+        const otherCases = getCombinations2(newCount);
+
+        for (let i = 0; i < otherCases.length; i++) {
+            for (let j = 0; j < arr[0].length; j++) {
+                combinations.push([ arr[0][j], ...otherCases[i] ]);
+            }
+        }
+
+        return combinations;
+    }
+};
+
 export const parseFileContents = (file: string): Record<number, number[]> => {
     const equations: Record<number, number[]> = {};
 
@@ -72,6 +96,52 @@ export const day7Part1 = (fileContents: string): number => {
                         break;
                     case '*':
                         equationSum *= number2;
+                        break;
+                    /* istanbul ignore next */
+                    default:
+                        throw new Error('Unknown operator!');
+                }
+            }
+
+            if (Number(sum) === equationSum) {
+                isSumPossible = true;
+                break;
+            }
+        }
+
+        if (isSumPossible) {
+            total += Number(sum);
+        }
+    }
+
+    return total;
+};
+
+export const day7Part2 = (fileContents: string): number => {
+    const equations = parseFileContents(fileContents);
+    const strs: any[] = [];
+    let total = 0;
+
+    for (const [ sum, numbers ] of Object.entries(equations)) {
+        const { length } = numbers;
+        const operators = getCombinations2(length - 1);
+        let isSumPossible = false;
+
+        for (let i = 0; i < operators.length; i++) {
+            let equationSum = numbers[0];
+
+            for (let j = 0; j < operators[i].length; j++) {
+                const number2 = numbers[j + 1];
+
+                switch(operators[i][j]) {
+                    case '+':
+                        equationSum += number2;
+                        break;
+                    case '*':
+                        equationSum *= number2;
+                        break;
+                    case '||':
+                        equationSum = Number(`${equationSum}${number2}`);
                         break;
                     /* istanbul ignore next */
                     default:
--- test/2024/day7.test.ts
+++ test/2024/day7.test.ts
@@ -1,5 +1,6 @@
 import {
     day7Part1,
+    day7Part2,
 } from '../../src/2024/day7';
 
 describe('day7 tests', () => {
@@ -30,6 +31,36 @@ describe('day7 tests', () => {
 #.........
 ......#...`.trim();
             expect(() => day7Part1(testInput)).toThrow();
+        });
+    });
+
+    describe('day7Part2 tests', () => {
+        it('Should return the number of positions in the guard\'s patrol path', () => {
+            const testInput = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`;
+            expect(day7Part2(testInput)).toBe(11387);
+        });
+
+        it('Should throw an error for invalid input', () => {
+            const testInput = `
+....#.....
+..........
+..........
+..#.......
+.#.....#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`.trim();
+            expect(() => day7Part2(testInput)).toThrow();
         });
     });
 });
