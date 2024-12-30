432c1039ccc037ae141752dda700a4d285115070
William Hurley <wjhurley@comcast.net>
2024-12-29 23:45:20 -0800

    Add solution and associated tests for 2024 Day 8 part 2

--- src/2024/day8.ts
+++ src/2024/day8.ts
@@ -81,3 +81,102 @@ export class Day8Part1 {
             .map(line => line.split(''));
     }
 }
+
+export class Day8Part2 {
+    private _antiNodes = 0;
+
+    private antennaMap: string[][];
+
+    private antennas: Record<string, Array<[number, number]>> = {};
+
+    constructor(fileContents: string) {
+        this.antennaMap = this.parseAntennaMap(fileContents);
+        this.mapAntennas();
+    }
+
+    public get antiNodes(): number {
+        if (this._antiNodes === 0) {
+            this.findAntiNodes();
+        }
+
+        return this._antiNodes;
+    }
+
+    private checkIfPositionIsValid(possibleAntiNode: [number, number]): boolean {
+        const minIndex = 0;
+        const maxIndex = this.antennaMap.length - 1;
+
+        return possibleAntiNode.every(num => num >= minIndex && num <= maxIndex);
+    }
+
+    private findAntiNodes(): void {
+        const possibleAntiNodes: Array<[number, number]> = [];
+        const validAntiNodes: Array<[number, number]> = [];
+
+        for (const frequency of Object.values(this.antennas)) {
+            if (frequency.length > 1) {
+                possibleAntiNodes.push(...frequency);
+            }
+
+            for (const [ index, [ y1, x1 ] ] of Object.entries(frequency)) {
+                const otherAntennas = [ ...frequency ];
+                otherAntennas.splice(Number(index), 1);
+
+                for (const [ y2, x2 ] of otherAntennas) {
+                    const [ diffY, diffX ] = [ y2 - y1, x2 - x1 ];
+                    let antiNode1Y = y1 - diffY;
+                    let antiNode1X = x1 - diffX;
+                    let antiNode2Y = y2 + diffY;
+                    let antiNode2X = x2 + diffX;
+
+                    while (
+                        this.checkIfPositionIsValid([ antiNode1Y, antiNode1X ])
+                        || this.checkIfPositionIsValid([ antiNode2Y, antiNode2X ])
+                    ) {
+                        if (this.checkIfPositionIsValid([ antiNode1Y, antiNode1X ])) {
+                            possibleAntiNodes.push([ antiNode1Y, antiNode1X ]);
+                        }
+
+                        if (this.checkIfPositionIsValid([ antiNode2Y, antiNode2X ])) {
+                            possibleAntiNodes.push([ antiNode2Y, antiNode2X ]);
+                        }
+
+                        antiNode1Y -= diffY;
+                        antiNode1X -= diffX;
+                        antiNode2Y += diffY;
+                        antiNode2X += diffX;
+                    }
+                }
+            }
+        }
+
+        for (const possibleAntiNode of possibleAntiNodes) {
+            const doesPositionAlreadyExist = validAntiNodes.some(
+                antiNode => isArrayEqual(antiNode, possibleAntiNode)
+            );
+
+            if (!doesPositionAlreadyExist) {
+                validAntiNodes.push(possibleAntiNode);
+            }
+        }
+
+        this._antiNodes = validAntiNodes.length;
+    }
+
+    private mapAntennas(): void {
+        for (const [ indexY, row ] of Object.entries(this.antennaMap)) {
+            for (const [ indexX, column ] of Object.entries(row)) {
+                if (/\w/.test(column)) {
+                    this.antennas[column] = this.antennas[column] || [];
+                    this.antennas[column].push([ Number(indexY), Number(indexX) ]);
+                }
+            }
+        }
+    }
+
+    private parseAntennaMap(file: string): string[][] {
+        return file
+            .split('\n')
+            .map(line => line.split(''));
+    }
+}
--- test/2024/day8.test.ts
+++ test/2024/day8.test.ts
@@ -1,11 +1,10 @@
 import {
     Day8Part1,
+    Day8Part2,
 } from '../../src/2024/day8';
 
 describe('day8 tests', () => {
-    describe('Day8Part1 tests', () => {
-        it('Should return the number of antiNodes in a given map', () => {
-            const testInput = `
+    const testInput = `
 ............
 ........0...
 .....0......
@@ -18,8 +17,18 @@ describe('day8 tests', () => {
 .........A..
 ............
 ............`.trim();
+
+    describe('Day8Part1 tests', () => {
+        it('Should return the number of antiNodes in a given map', () => {
             const day8Part1 = new Day8Part1(testInput);
             expect(day8Part1.antiNodes).toBe(14);
+        });
+    });
+
+    describe('Day8Part2 tests', () => {
+        it('Should return the number of antiNodes in a given map', () => {
+            const day8Part2 = new Day8Part2(testInput);
+            expect(day8Part2.antiNodes).toBe(34);
         });
     });
 });
