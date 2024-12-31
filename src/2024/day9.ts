ad80641f1bab4456e20ee00a8513bc2840e2d02d
William Hurley <wjhurley@comcast.net>
2024-12-31 01:45:32 -0800

    Add solution and associated tests for 2024 Day 9 part 2

--- src/2024/day9.ts
+++ src/2024/day9.ts
@@ -85,3 +85,97 @@ export class Day9Part1 {
         return parsedDiskMap;
     }
 }
+
+export class Day9Part2 {
+    private _checkSum = 0;
+
+    private diskMap: string[][];
+
+    constructor(fileContents: string) {
+        this.diskMap = this.parseDiskMap(fileContents);
+        this.moveFileBlocks();
+    }
+
+    public get checkSum(): number {
+        if (this._checkSum === 0) {
+            this.calculateCheckSum();
+        }
+
+        return this._checkSum;
+    }
+
+    private calculateCheckSum(): void {
+        const flatMap = this.diskMap.flat();
+        let runningTotal = 0;
+
+        for (const [ index, block ] of Object.entries(flatMap)) {
+            /* istanbul ignore next */
+            if (block === '.') {
+                continue;
+            }
+
+            runningTotal += Number(index) * Number(block);
+        }
+
+        this._checkSum = runningTotal;
+    }
+
+    private moveFileBlocks(): void {
+        const blocks = [ ...this.diskMap ];
+
+        for (let index = 0; index < blocks.length; index++) {
+            const block = blocks[index];
+
+            if (block.every(val => /\d+/.test(val))) {
+                continue;
+            }
+
+            for (let i = blocks.length - 1; i >= 0; i--) {
+                const fileBlock = blocks[i];
+
+                if (
+                    fileBlock.every(val => val === '.')
+                    || block.length < fileBlock.length
+                    || i < index
+                ) {
+                    continue;
+                }
+
+                const blockNewLength = block.length - fileBlock.length;
+
+                // Same size, no need to splice and create new free space array
+                if (blockNewLength === 0) {
+                    blocks[i] = block;
+                    blocks[index] = fileBlock;
+                } else {
+                    blocks[i] = new Array<string>(block.length - blockNewLength).fill('.');
+                    blocks[index] = new Array<string>(blockNewLength).fill('.');
+                    blocks.splice(index, 0, fileBlock);
+                }
+
+                break;
+            }
+        }
+
+        this.diskMap = blocks;
+    }
+
+    private parseDiskMap(file: string): string[][] {
+        const digits = file.split('');
+        let fileId = 0;
+        let parsedDiskMap: string[][] = [];
+
+        for (const [ index, digit ] of Object.entries(digits)) {
+            if (Number(index) % 2 === 0) {
+                const arr = new Array<string>(Number(digit)).fill(`${fileId}`);
+                parsedDiskMap.push(arr);
+                fileId++;
+            } else {
+                const arr = new Array<string>(Number(digit)).fill('.');
+                parsedDiskMap.push(arr);
+            }
+        }
+
+        return parsedDiskMap.filter(arr => arr.length > 0);
+    }
+}
--- test/2024/day9.test.ts
+++ test/2024/day9.test.ts
@@ -1,5 +1,6 @@
 import {
     Day9Part1,
+    Day9Part2,
 } from '../../src/2024/day9';
 
 describe('day9 tests', () => {
@@ -14,6 +15,20 @@ describe('day9 tests', () => {
             const testInput = '9371657678826024995120881287893479';
             const day9Part1 = new Day9Part1(testInput);
             expect(day9Part1.checkSum).toBe(44861);
+        });
+    });
+
+    describe('Day9Part2 tests', () => {
+        it('Should return the correct checksum for a given diskMap', () => {
+            const testInput = '2333133121414131402';
+            const day9Part2 = new Day9Part2(testInput);
+            expect(day9Part2.checkSum).toBe(2858);
+        });
+
+        it('Should return the correct checksum for a larger diskMap', () => {
+            const testInput = '9371657678826024995120881287893479';
+            const day9Part2 = new Day9Part2(testInput);
+            expect(day9Part2.checkSum).toBe(55574);
         });
     });
 });
