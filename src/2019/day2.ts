3dbb38396d946cb3afb35bc069df8bd848620dc8
William Hurley <wjhurley@comcast.net>
2025-01-13 22:12:16 -0800

    Refactor 2019 Day 2 and add associated tests

--- src/2019/day2.ts
+++ src/2019/day2.ts
@@ -4,62 +4,82 @@ import * as path from 'path';
 
 config();
 
-export const day2Part1 = (noun: number, verb: number): number => {
-    // First, get the contents of the input file for today's puzzle
-    const inputFilePath = path.join(process.env.TQ_AOC_INPUT_FOLDER ?? __dirname, 'day2.txt');
-    const inputFileContents = fs.readFileSync(inputFilePath, 'utf-8');
+export class Day2Part2 {
+    private readonly input: number[];
 
-    // Next, let's put every number in the input file into an array
-    const input = inputFileContents.split(',').map(val => Number(val));
-    // If the last line is blank, remove it
-    if (Number.isNaN(input[input.length - 1])) {
-        input.pop();
+    private readonly outputToFind: number;
+
+    private tempInput: number[];
+
+    constructor(outputToFind: number) {
+        // First, get the contents of the input file for today's puzzle
+        const inputFilePath = path.join(__dirname, '../../input/2019/day2.txt');
+        const inputFileContents = fs.readFileSync(inputFilePath, 'utf-8');
+
+        // Next, let's put every number in the input file into an array
+        this.input = inputFileContents
+            .split(',')
+            .filter(val => val !== '')
+            .map(Number);
+
+        this.outputToFind = outputToFind;
     }
 
-    const handleIntcodeProgram = ([opcode, firstPos, secondPos, outputPos]: number[]): boolean => {
+    private day2Part1(noun: number, verb: number): number {
+        this.tempInput = [ ...this.input ];
+
+        // Restore program to "1202 program alarm"
+        this.tempInput[1] = noun;
+        this.tempInput[2] = verb;
+
+        for (let i = 0; i < this.tempInput.length; i += 4) {
+            const isProgramFinished = this.handleIntcodeProgram(this.tempInput.slice(i, i + 4));
+
+            if (isProgramFinished) {
+                break;
+            }
+        }
+
+        return this.tempInput[0];
+    }
+
+    private handleIntcodeProgram(slice: number[]): boolean {
+        const [
+            opcode,
+            firstPos,
+            secondPos,
+            outputPos,
+        ] = slice;
+
         if (opcode === 99) {
             return true;
         }
 
-        const firstVal = input[firstPos];
-        const secondVal = input[secondPos];
-        const outputVal = opcode === 1
+        const firstVal = this.tempInput[firstPos];
+        const secondVal = this.tempInput[secondPos];
+
+        this.tempInput[outputPos] = opcode === 1
             ? firstVal + secondVal
             : firstVal * secondVal;
-        input[outputPos] = outputVal;
 
         return false;
-    };
-    // Restore program to "1202 program alarm"
-    input[1] = noun;
-    input[2] = verb;
-
-    for (let i = 0; i < input.length; i += 4) {
-        const isProgramFinished = handleIntcodeProgram(input.slice(i, i + 4));
-
-        if (isProgramFinished) {
-            break;
-        }
     }
 
-    return input[0];
-};
+    public get output(): number {
+        let noun: number;
+        let verb: number;
 
-export const day2Part2 = (): number => {
-    const outputToFind = 19690720;
-    let noun: number;
-    let verb: number;
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
+                if (this.day2Part1(i, j) === this.outputToFind) {
+                    noun = i;
+                    verb = j;
 
-    for (let i = 0; i < 100; i++) {
-        for (let j = 0; j < 100; j++) {
-            if (day2Part1(i, j) === outputToFind) {
-                noun = i;
-                verb = j;
-
-                return noun * 100 + verb;
+                    return noun * 100 + verb;
+                }
             }
         }
-    }
 
-    return 0;
-};
+        return 0;
+    }
+}
---
+++ test/2019/day2.test.ts
@@ -0,0 +1,15 @@
+import { Day2Part2 } from '../../src/2019/day2';
+
+describe('day2 tests', () => {
+    describe('Day2Part2 tests', () => {
+        it('Should return the return the correct value for position 0 using the provided value', () => {
+            const day2Part2 = new Day2Part2(19690720);
+            expect(day2Part2.output).toBe(5064);
+        });
+
+        it('Should return 0 if the provided value cannot be found at position 0', () => {
+            const day2Part2 = new Day2Part2(100);
+            expect(day2Part2.output).toBe(0);
+        });
+    });
+});
