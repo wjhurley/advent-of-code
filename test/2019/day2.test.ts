2085a738a808438a96f325966e53475f19b9bce8
William Hurley <wjhurley@comcast.net>
2025-01-25 17:55:16 -0800

    Fix typo in test name and set badges for completed 2019 days

--- test/2019/day2.test.ts
+++ test/2019/day2.test.ts
@@ -2,7 +2,7 @@ import { Day2Part2 } from '../../src/2019/day2';
 
 describe('day2 tests', () => {
     describe('Day2Part2 tests', () => {
-        it('Should return the return the correct value for position 0 using the provided value', () => {
+        it('Should return the correct value for position 0 using the provided value', () => {
             const day2Part2 = new Day2Part2(19690720);
             expect(day2Part2.output).toBe(5064);
         });
