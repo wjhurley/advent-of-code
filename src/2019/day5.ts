c0361eb3a00d06036b7c55f2a4c8208b58d26e15
William Hurley <wjhurley@comcast.net>
2025-02-12 18:16:40 -0800

    Add solution and associated tests for 2019 Day 5 part 2

--- src/2019/day5.ts
+++ src/2019/day5.ts
@@ -19,7 +19,6 @@ export const day5Part1 = (fileContents: string, inputValue: number): number => {
     let increment = 0;
 
     for (let i = 0; i < input.length; i += increment) {
-        /* istanbul ignore next */
         if (input[i] === 99) {
             break;
         }
@@ -78,6 +77,106 @@ export const day5Part1 = (fileContents: string, inputValue: number): number => {
             /* istanbul ignore next */
             default:
                 throw new Error(`Invalid or unsupported opCode (Given: ${opCode})`);
+        }
+    }
+
+    throw new Error('Output value not found!');
+};
+
+export const day5Part2 = (fileContents: string, inputValue: number): number => {
+    const input = parseFileContents(fileContents);
+    const threeParameterOpCodes = [
+        '1',
+        '2',
+        '7',
+        '8',
+    ];
+    let increment = 0;
+
+    for (let i = 0; i < input.length; i += increment) {
+        if (input[i] === 99) {
+            break;
+        }
+
+        const [
+            _parameter3,
+            parameter2,
+            parameter1,
+            _,
+            opCode,
+        ] = input[i]
+            .toString()
+            .padStart(5, '0');
+
+        let firstVal = 0;
+        let secondVal = 0;
+        let outputPos = 0;
+
+        if (threeParameterOpCodes.includes(opCode)) {
+            increment = 4;
+
+            firstVal = parameter1 === '0'
+                ? input[input[i + 1]]
+                : input[i + 1];
+            secondVal = parameter2 === '0'
+                ? input[input[i + 2]]
+                : input[i + 2];
+            outputPos = input[i + 3];
+        } else if (opCode === '3' || opCode === '4') {
+            increment = 2;
+
+            if (opCode === '3') {
+                input[input[i + 1]] = inputValue;
+            } else {
+                const value = parameter1 === '0'
+                    ? input[input[i + 1]]
+                    : input[i + 1];
+
+                if (value !== 0) {
+                    return value;
+                }
+            }
+
+            continue;
+        } else if (opCode === '5' || opCode === '6') {
+            increment = 3;
+
+            firstVal = parameter1 === '0'
+                ? input[input[i + 1]]
+                : input[i + 1];
+            secondVal = parameter2 === '0'
+                ? input[input[i + 2]]
+                : input[i + 2];
+
+            if (
+                (opCode === '5' && firstVal !== 0)
+                || (opCode === '6' && firstVal === 0)
+            ) {
+                increment = 0;
+                i = secondVal;
+            }
+
+            continue;
+        } else {
+            throw new Error(`Invalid opCode! (Given: ${input[i]}`);
+        }
+
+        switch (true) {
+            case opCode === '1':
+                input[outputPos] = firstVal + secondVal;
+                break;
+            case opCode === '2':
+                input[outputPos] = firstVal * secondVal;
+                break;
+            case opCode === '7':
+                input[outputPos] = firstVal < secondVal ? 1 : 0;
+                break;
+            case opCode === '8':
+                input[outputPos] = firstVal === secondVal ? 1 : 0;
+                break;
+            /* istanbul ignore next */
+            default:
+                throw new Error(`Invalid or unsupported opcode (Given: ${opCode})`);
         }
     }
 
--- test/2019/day5.test.ts
+++ test/2019/day5.test.ts
@@ -1,4 +1,7 @@
-import { day5Part1 } from '../../src/2019/day5';
+import {
+    day5Part1,
+    day5Part2,
+} from '../../src/2019/day5';
 
 describe('day5 tests', () => {
     describe('day5Part1 tests', () => {
@@ -16,6 +19,34 @@ describe('day5 tests', () => {
 
         it('Should throw an error if an invalid opCode exists in the diagnostic program', () => {
             expect(() => day5Part1('1005,4,3,4,33', 1)).toThrow();
+        });
+    });
+
+    describe('day5Part2 tests', () => {
+        const testInput = '3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99';
+
+        it('Should return the correct output value for the diagnostic program and input provided 1', () => {
+            expect(day5Part2('3,13,1,0,0,0,8,13,14,13,4,13,99,-1,8', 8)).toBe(1);
+        });
+
+        it('Should return the correct output value for the diagnostic program and input provided 2', () => {
+            expect(day5Part2(testInput, 7)).toBe(999);
+        })
+
+        it('Should return the correct output value for the diagnostic program and input provided 3', () => {
+            expect(day5Part2(testInput, 8)).toBe(1000);
+        })
+
+        it('Should return the correct output value for the diagnostic program and input provided 4', () => {
+            expect(day5Part2(testInput, 9)).toBe(1001);
+        })
+
+        it('Should throw an error because no non-zero output value is found', () => {
+            expect(() => day5Part2('3,9,7,9,10,9,4,9,99,-1,8', 8)).toThrow();
+        });
+
+        it('Should throw an error if an invalid opCode exists in the diagnostic program', () => {
+            expect(() => day5Part2('5,1,5,100,4,3,4,33', 1)).toThrow();
         });
     });
 });
